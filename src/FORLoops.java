248e7b6b47e862bbab03251b7ae4ed1a365529e1
apaara11 <league@WorthyMini-6.attlocal.net>
2017-01-27 16:58:27 -0800

    KAUR 6

---
+++ src/FORLoops.java
@@ -0,0 +1,13 @@
+
+public class FORLoops {
+public static void main(String[] args) {
+	for(int k = 0; k < 101; k++) {
+	System.out.print(k+" ");	
+	}
+	System.out.println();
+	
+	for (int i = 100; i >or= 0; i--) {
+		System.out.print(i+" ");
+	}
+}
+}
