bdacc33d0d19cdb0640c100bcde8d204ad63dd31
LEAUGE COMPUTER <DO NOT CHANGE THIS/>
2017-03-21 16:58:53 -0700

    Grandma and Grandpa comes!

---
+++ src/Adventure.java
@@ -0,0 +1,34 @@
+import javax.swing.JOptionPane;
+
+public class Adventure {
+public static void main(String[] args) {
+	 JOptionPane.showMessageDialog(null, " Once upon a time, there lived a famous racecar driver named Devyn.\n"
+			 + "She was smart and brave. You choose her life path");
+	 
+	int operation = JOptionPane.showOptionDialog(null, " First she has a race, but then she has to go to a grand opening of a place.\n"
+			+"Choose one way for her to get there",
+			"Adventure transportation ", 0,
+			JOptionPane.INFORMATION_MESSAGE, null, new String[] { "limo", "race car" },
+			null);
+	if (operation==0){
+		JOptionPane.showMessageDialog(null, "Good Choice!");
+		
+		int limo = JOptionPane.showOptionDialog(null, "What color would you like her limo to be?",
+				"Adventure transportation ", 0,
+				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "pink", "white" },
+				null);
+		if(limo==0){
+			JOptionPane.showMessageDialog(null, "GOOD CHOICE!");
+		}
+		else{
+			JOptionPane.showMessageDialog(null, "Bad choice. No one can tell it is her. Everyone used the same color for their limo.");
+		}
+	}
+	if(operation==1){
+		JOptionPane.showMessageDialog(null, "Sorry, she got arrested for speeding and not being tight on the turns. Her carrer is OVER.");
+	}
+
+	
+}
+}
+
--- src/FORLoops.java
+++ src/FORLoops.java
@@ -1,4 +1,4 @@
-
+                       
 public class FORLoops {
 public static void main(String[] args) {
 	for(int k = 0; k < 101; k++) {
@@ -6,8 +6,21 @@ public static void main(String[] args) {
 	}
 	System.out.println();
 	
-	for (int i = 100; i >or= 0; i--) {
+	for (int i = 100; i >= 0; i--) {
 		System.out.print(i+" ");
 	}
+	System.out.println();
+	
+	for(int k= 2; k<101; k+=2){
+	System.out.print(k+" ");
+	}
+	System.out.println();
+	
+	for(int b=1; b<100; b+=2){
+		System.out.print(b+" ");
+	}
+	
 }
 }
+
+
