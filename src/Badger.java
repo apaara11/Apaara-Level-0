c8dc219a16be1619c9d92bbc4414ab75badfd0ae
test <league@WTS15.attlocal.net>
2017-04-04 16:58:01 -0700

    4/4/17

---
+++ src/Badger.java
@@ -0,0 +1,24 @@
+import javax.swing.JOptionPane;
+
+public class Badger {
+	public static void main(String[] args) {
+		for (int kaur = 0; kaur < 4; kaur++) {
+			for (int i = 0; i < 10; i++) {
+				System.out.print("badger ");
+			}
+
+			
+			if(kaur==3){
+				System.out.println("Argh!Snake,  a snake!");
+				System.out.println("Snaaake! A snaaaake, ooh it's a snake");
+			}
+			else{
+				for (int b = 0; b < 2; b++) {
+					System.out.print("mushroom  ");
+				}
+				System.out.println("ah");
+			}
+			
+		}
+	}
+}
--- src/SleepyHead.java
+++ src/SleepyHead.java
@@ -1,23 +1,32 @@
+
 // Copyright Wintriss Technical Schools 2013
 import javax.swing.JOptionPane;
 
 public class SleepyHead {
 
 	public static void main(String[] args) {
-		
+
 		boolean isWeekday, isVacation;
-		
+
 		/*
 		 * Ask the user for these values using
 		 * JOptionPane.showConfirmDialog(null, "is it a weekday?", "",
-		 * JOptionPane.YES_NO_OPTION);
+		 * JOptionPane.YES_NO_OPTION); /* Print “sleep in” if it is a vacation
+		 * or a weekend. If it’s a weekday, print “get up lazybones!” If it is a
+		 * weekday, and we are on vacation, print “sleep in”.
 		 */
-	
-
-		/*
-		 * Print “sleep in” if it is a vacation or a weekend. If it’s a weekday,
-		 * print “get up lazybones!” If it is a weekday, and we are on vacation,
-		 * print “sleep in”.
-		 */
+		int answer = JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Question", JOptionPane.YES_NO_OPTION);
+		if (answer == JOptionPane.YES_OPTION) {
+			int respond = JOptionPane.showConfirmDialog(null, "Is it a vacation?", "Another Question", JOptionPane.YES_NO_OPTION);
+			if (respond==JOptionPane.YES_OPTION){
+				JOptionPane.showMessageDialog(null, "sleep in");
+			}
+			else{
+				JOptionPane.showMessageDialog(null, "Get up lazybones!");
+			}
+		}
+		else{
+			JOptionPane.showMessageDialog(null, "sleep in.");
+		}
 	}
 }
