06876b3937627ded8411a1d68e40716400df3be7
Love4653 <league@iMac-2.attlocal.net>
2017-04-25 16:54:08 -0700

    4/25/17

---
+++ src/Strings.java
@@ -0,0 +1,24 @@
+
+public class StringMethods1 {
+	public static void main(String[] args) {
+		// 1. Create a String variable and initialize it to whatever you want
+		String kaur= "Hello";
+		// 2. Print the 3rd char of your String to the console.
+		//    HINT: .charAt
+		kaur.charAt(3);
+		// 3. Print the length of your String to the console.
+		//    HINT: .length
+
+		// 4. Using a for loop, print one char at time to the console.
+		//    HINT: use .length() to determine how many loops
+
+		
+		// 5. Pick a char inside your String, and use a loop to determine
+		//    what position/index in the String the char is located.
+		//    Print the char's position to the console.
+		//    EXAMPLE: if your string is "abc" and you are searching 
+		//             for char 'b', then print "b is at index 1"
+		
+	}
+}
+
